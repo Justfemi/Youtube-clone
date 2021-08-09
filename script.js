ebb56a2749a3dc7b34d12e13b8a2edf6b95d5620
Justfemi <ajibadeolufemi94@gmail.com>
2021-08-09 20:55:41 +0100

    ft:added play video page

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// alert('Script file connected');
+let menuIcon = document.querySelector('.menu-icon');
+let sidebar = document.querySelector('.sidebar');
+let container = document.querySelector('.container');
+
+menuIcon.addEventListener('click', () => {
+    sidebar.classList.toggle('small-sidebar');
+    container.classList.toggle('large-container');
+})
